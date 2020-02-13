967818e6a34b9beeccd4d8ba0fc6cbb3c18d9b68
EC2 Default User <ec2-user@ip-172-31-41-66.ec2.internal>
2020-02-13 14:58:33 +0000

    ajax fluency

---
+++ app.js
@@ -0,0 +1,33 @@
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = () => {
+        if(xhr.readyState == 4 && xhr.status == 200){
+            let reqObj = JSON.parse(xhr.response);
+            console.log(reqObj.data.slice(0,5)); 
+        }
+ };
+ 
+xhr.open('GET', 'https://reqres.in/api/users');
+xhr.send();
+
+function getLog(urlStr){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState == 4 && xhr.status == 200){
+            console.log(JSON.parse(xhr.response)); 
+        }
+     };
+    xhr.open('GET', urlStr);
+    xhr.send();
+}
+
+function get(url, callback){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () =>{
+        if (xhr.readyState == 4 && xhr.status == 200){
+            callback(url);
+        }
+    };
+    xhr.open('GET', url);
+    xhr.send();
+}
